a506ffb8c063e616d8dc601392cad6ca183d80dd
Zarkonnen <david.stark@zarkonnen.com>
2013-04-07 12:30:12 +0200

    Fixed furniture heights.

--- src/com/zarkonnen/FurnitureStore.java
+++ src/com/zarkonnen/FurnitureStore.java
@@ -25,84 +25,84 @@ public enum FurnitureStore {
 		}
 	},
 	TABLE(Location.FLOOR, 180, 90, 0),
-	CANDLE_TABLE(Location.FLOOR, 90, 130, 40) {
+	CANDLE_TABLE(Location.FLOOR, 180, 130, 40) {
 		@Override
 		public void assemble(Level l, int x, int y) {
 			TABLE.assemble(l, x, y + 40);
 			l.walls.add(new Candle( x + 10 + l.r.nextInt(w - 20), y + 20, l.power));
 		}
 	},
-	FOUNTAIN(Location.FLOOR, 120, 120, 0) {
+	FOUNTAIN(Location.FLOOR, 120, 320, 0) {
 		@Override
 		public void assemble(Level l, int x, int y) {
-			l.walls.add(new Wall(x, y + 60, 120, 60).floor());
-			l.walls.add(new Fountainhead(x + 60, y, l.power, Element.ICE, Clr.BLUE));
+			l.walls.add(new Wall(x, y + 260, 120, 60).floor());
+			l.walls.add(new Fountainhead(x + 60, y + 200, l.power, Element.ICE, Clr.BLUE));
 		}
 	},
-	HOLY_FOUNTAIN(Location.FLOOR, 120, 120, 0) {
+	HOLY_FOUNTAIN(Location.FLOOR, 120, 320, 0) {
 		@Override
 		public void assemble(Level l, int x, int y) {
-			l.walls.add(new Wall(x, y + 60, 120, 60).floor());
-			l.walls.add(new Fountainhead(x + 60, y, l.power, Element.BLESSED, Element.BLESSED.tint));
+			l.walls.add(new Wall(x, y + 260, 120, 60).floor());
+			l.walls.add(new Fountainhead(x + 60, y + 200, l.power, Element.BLESSED, Element.BLESSED.tint));
 		}
 	},
-	UNHOLY_FOUNTAIN(Location.FLOOR, 120, 120, 0) {
+	UNHOLY_FOUNTAIN(Location.FLOOR, 120, 320, 0) {
 		@Override
 		public void assemble(Level l, int x, int y) {
-			l.walls.add(new Wall(x, y + 60, 120, 60).floor());
-			l.walls.add(new Fountainhead(x + 60, y, l.power, Element.CURSED, Element.CURSED.tint));
+			l.walls.add(new Wall(x, y + 260, 120, 60).floor());
+			l.walls.add(new Fountainhead(x + 60, y + 200, l.power, Element.CURSED, Element.CURSED.tint));
 		}
 	},
 	SHELF(Location.WALL, 120, 25, 0),
 	PLATFORM(Location.WALL, 400, 30, 0),
-	BOOKSHELF(Location.WALL, 120, 50, 25) {
+	BOOKSHELF(Location.WALL, 120, 105, 80) {
 		@Override
 		public void assemble(Level l, int x, int y) {
-			SHELF.assemble(l, x, y + 25);
+			SHELF.assemble(l, x, y + 80);
 			int books = 2 + l.r.nextInt(5);
 			int bookStart = 5 + l.r.nextInt(20);
 			for (int i = 0; i < books; i++) {
-				l.walls.add(new Book(x + bookStart + i * 15, y + 25, l));
+				l.walls.add(new Book(x + bookStart + i * 15, y + 80, l));
 			}
 		}
 	},
-	EVIL_BOOKSHELF(Location.WALL, 120, 70, 45) {
+	EVIL_BOOKSHELF(Location.WALL, 120, 125, 100) {
 		@Override
 		public void assemble(Level l, int x, int y) {
-			SHELF.assemble(l, x, y + 45);
-			l.walls.add(new EvilBook(x + l.r.nextInt(80) + 10, y + 45, l));
+			SHELF.assemble(l, x, y + 100);
+			l.walls.add(new EvilBook(x + l.r.nextInt(80) + 10, y + 100, l));
 		}
 	},
-	DOGSHELF(Location.WALL, 120, 65, 40) {
+	DOGSHELF(Location.WALL, 120, 105, 80) {
 		@Override
 		public void assemble(Level l, int x, int y) {
-			SHELF.assemble(l, x, y + 40);
+			SHELF.assemble(l, x, y + 80);
 			int start = 5 + l.r.nextInt(30);
 			while (start < 80) {
-				l.walls.add(new ChinaDog(x + start, y + 40));
+				l.walls.add(new ChinaDog(x + start, y + 80));
 				start += 33 + l.r.nextInt(30);
 			}
 		}
 	},
-	BEESHELF(Location.WALL, 120, 125, 0) {
+	BEESHELF(Location.WALL, 120, 150, 0) {
 		@Override
 		public void assemble(Level l, int x, int y) {
-			SHELF.assemble(l, x, y + 100);
-			l.walls.add(new BeeJar(x + l.r.nextInt(10) + 5, y + 100, l.power));
+			SHELF.assemble(l, x, y + 125);
+			l.walls.add(new BeeJar(x + l.r.nextInt(10) + 5, y + 125, l.power));
 		}
 	},
-	ACIDSHELF(Location.WALL, 120, 90, 0) {
+	ACIDSHELF(Location.WALL, 120, 75, 50) {
+		@Override
+		public void assemble(Level l, int x, int y) {
+			SHELF.assemble(l, x, y + 50);
+			l.walls.add(new Bottle(x + l.r.nextInt(90) + 5, y + 50, l.power, 0.2, null));
+		}
+	},
+	MEDISHELF(Location.WALL, 120, 75, 50) {
 		@Override
 		public void assemble(Level l, int x, int y) {
 			SHELF.assemble(l, x, y + 30);
-			l.walls.add(new Bottle(x + l.r.nextInt(90) + 5, y + 30, l.power, 0.2, null));
-		}
-	},
-	MEDISHELF(Location.WALL, 120, 90, 0) {
-		@Override
-		public void assemble(Level l, int x, int y) {
-			SHELF.assemble(l, x, y + 30);
-			l.walls.add(new Bottle(x + l.r.nextInt(90) + 5, y + 30, l.power, -0.8, HEAL));
+			l.walls.add(new Bottle(x + l.r.nextInt(90) + 5, y + 50, l.power, -0.8, HEAL));
 		}
 	},
 	TORCH(Location.WALL, 8, 24) {
