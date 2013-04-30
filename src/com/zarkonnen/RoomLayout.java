66191a2e27468b7789d4ad2fb54681fb0d2f5607
Zarkonnen <david.stark@zarkonnen.com>
2013-04-30 12:58:18 +0200

    Room layout IO.

---
+++ src/com/zarkonnen/RoomLayout.java
@@ -0,0 +1,94 @@
+package com.zarkonnen;
+
+import com.zarkonnen.catengine.util.Pt;
+import com.zarkonnen.catengine.util.Utils;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class RoomLayout {
+	public int background;
+	public boolean[] window = new boolean[Level.LVL_W * Level.GRID_SIZE / 512 + 2];
+	public ArrayList<Utils.Pair<WallDecoType, Pt>> decos = new ArrayList<Utils.Pair<WallDecoType, Pt>>();
+	public ArrayList<Utils.Pair<FurnitureStore, Pt>> furniture = new ArrayList<Utils.Pair<FurnitureStore, Pt>>();
+	
+	RoomLayout ln(PrintWriter pw, String ln) {
+		pw.println(ln);
+		return this;
+	}
+	
+	RoomLayout ln(PrintWriter pw, int ln) {
+		pw.println(ln);
+		return this;
+	}
+	
+	RoomLayout ln(PrintWriter pw, double ln) {
+		pw.println(ln);
+		return this;
+	}
+	
+	RoomLayout ln(PrintWriter pw, boolean ln) {
+		pw.println(ln);
+		return this;
+	}
+	
+	public void write(PrintWriter pw) {
+		ln(pw, "RoomLayout").ln(pw, 1).ln(pw, background);
+		ln(pw, window.length);
+		for (boolean w : window) { ln(pw, w); }
+		for (Utils.Pair<WallDecoType, Pt> d : decos) {
+			ln(pw, d.a.name()).ln(pw, d.b.x).ln(pw, d.b.y);
+		}
+		for (Utils.Pair<FurnitureStore, Pt> d : furniture) {
+			ln(pw, d.a.name()).ln(pw, d.b.x).ln(pw, d.b.y);
+		}
+	}
+	
+	public static RoomLayout read(BufferedReader r) throws Exception {
+		if (!r.readLine().equals("RoomLayout")) {
+			throw new Exception("Not a RoomLayout");
+		}
+		if (i(r) > 1) {
+			throw new Exception("Version too new");
+		}
+		int nWins = i(r);
+		RoomLayout rl = new RoomLayout();
+		rl.window = new boolean[nWins];
+		for (int i = 0; i < nWins; i++) {
+			rl.window[i] = b(r);
+		}
+		String type;
+		while ((type = r.readLine()) != null) {
+			boolean success = false;
+			try {
+				WallDecoType wdt = WallDecoType.valueOf(type);
+				rl.decos.add(new Utils.Pair<WallDecoType, Pt>(wdt, new Pt(d(r), d(r))));
+				success = true;
+			} catch (Exception e) {}
+			try {
+				FurnitureStore fs = FurnitureStore.valueOf(type);
+				rl.furniture.add(new Utils.Pair<FurnitureStore, Pt>(fs, new Pt(d(r), d(r))));
+				success = true;
+			} catch (Exception e) {}
+			if (!success) {
+				throw new Exception("Unknown type: " + type);
+			}
+		}
+		return rl;
+	}
+	
+	public static int i(BufferedReader r) throws IOException {
+		return Integer.parseInt(r.readLine());
+	}
+	
+	public static boolean b(BufferedReader r) throws IOException {
+		return Boolean.parseBoolean(r.readLine());
+	}
+	
+	public static double d(BufferedReader r) throws IOException {
+		return Double.parseDouble(r.readLine());
+	}
+	
+	public RoomLayout() {}
+}
